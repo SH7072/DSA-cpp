50166a8c583e9ac69c34e0b1f4b14f5109d72b6b
Shubham Chandwani <chandwanishubham@gmail.com>
2022-05-07 03:55:06 +0530

    'changes'

--- OOP/encapsulation.cpp
+++ OOP/encapsulation.cpp
@@ -17,6 +17,12 @@ public:
     {
         cout << b << endl;
     }
+    void set_a(int s){
+        a=s;
+    }
+    int get_a(){
+       return a; 
+    }
 };
 
 int main()
@@ -25,6 +31,9 @@ int main()
 
     // obj1.funca();  // cant access since it is a private member of class
     obj1.funcb();     // can be access since it is a public member of class
+
+    obj1.set_a(20);
+   cout<<obj1.get_a();
 
     return 0;
 }
