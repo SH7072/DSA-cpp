33ceed9c5325757a75915059302e838d469a9ccb
Shubham Chandwani <chandwanishubham@gmail.com>
2022-05-10 23:58:38 +0530

    Ques Google kickstart

--- Arrays/recordBreaker.cpp
+++ Arrays/recordBreaker.cpp
@@ -2,23 +2,28 @@
 
 using namespace std;
 
-int recordBreaker(int arr[],int n){
-    int recBr=0;
-    int max1=INT_MIN;
-    
+int recordBreaker(int arr[], int n)
+{
+    int recBr = 0;
+    int max1 = INT_MIN;
+   
     for (int i = 0; i < n; i++)
     {
-        max1=max(max1,arr[i]);
-        if (i==n-1)
+
+        if ((i!=n-1)&&(arr[i] > max1) && (arr[i] > arr[i+1]))
         {
             recBr++;
-        }else if (i!=n-1&&arr[i+1]<max1)
+        }
+        else if (i==n-1&& arr[i]>max1)
         {
             recBr++;
-        }    
+        }
+        max1 = max(max1, arr[i]);
+        
+        
     }
-    
-    return recBr;        
+
+    return recBr;
 }
 
 int main()
@@ -26,11 +31,12 @@ int main()
 
     int n;
     cin >> n;
-    int arr[n+1];
+    int arr[n];
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
-    cout<<recordBreaker(arr,n+1);
+    
+    cout << recordBreaker(arr, n );
     return 0;
 }
