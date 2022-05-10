16db719a86edb47892c4cb81d39432a2a653efdd
Shubham Chandwani <chandwanishubham@gmail.com>
2022-05-11 00:32:34 +0530

    commit

--- Arrays/recordBreaker.cpp
+++ Arrays/recordBreaker.cpp
@@ -6,21 +6,19 @@ int recordBreaker(int arr[], int n)
 {
     int recBr = 0;
     int max1 = INT_MIN;
-   
+
     for (int i = 0; i < n; i++)
     {
 
-        if ((i!=n-1)&&(arr[i] > max1) && (arr[i] > arr[i+1]))
+        if ((i != n - 1) && (arr[i] > max1) && (arr[i] > arr[i + 1]))
         {
             recBr++;
         }
-        else if (i==n-1&& arr[i]>max1)
+        else if (i == n - 1 && arr[i] > max1)
         {
             recBr++;
         }
         max1 = max(max1, arr[i]);
-        
-        
     }
 
     return recBr;
@@ -28,15 +26,19 @@ int recordBreaker(int arr[], int n)
 
 int main()
 {
-
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
+    int k;
+    cin >> k;
+    for (int i = 0; i < k; i++)
     {
-        cin >> arr[i];
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+    cout <<"Case #"<<i+1<<": "<< recordBreaker(arr, n)<<endl;
     }
-    
-    cout << recordBreaker(arr, n );
+
     return 0;
 }
