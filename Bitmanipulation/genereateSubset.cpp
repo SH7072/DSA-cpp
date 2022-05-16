2daceae0774cf9c82ac228fdf9109ea07e92da0a
Shubham Chandwani <chandwanishubham@gmail.com>
2022-05-16 23:05:20 +0530

    bit manipulation

---
+++ Bitmanipulation/genereateSubset.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void generateSubset(int n,char arr[])
+{
+    for (int i = 0; i < (1<<n); i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i&(1<<j))
+            {
+                cout<<arr[j]<<" ";
+            }
+        }
+            cout<<endl;
+        
+    }
+    
+}
+int main()
+{
+    int n;
+    cin >> n;
+    char arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+     cout<<endl;
+    generateSubset(n,arr);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout<<arr[i];
+    // }
+    return 0;
+}
