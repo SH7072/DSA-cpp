93f2ca9b2a2505f9c132fd41a9e2eee3d0ab87b3
Shubham Chandwani <chandwanishubham@gmail.com>
2022-05-18 23:47:49 +0530

    ques

---
+++ Bitmanipulation/findUnique2.cpp
@@ -0,0 +1,28 @@
+// Write a program to find 2 unique numbers in an array where all numbers
+// except two, are present twice.
+#include <bits/stdc++.h>
+
+using namespace std;
+int getSetBit(int n){
+    
+}
+void fnd2unique(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        x=x^arr[i];
+    }
+    
+}
+int main()
+{
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    return 0;
+}
