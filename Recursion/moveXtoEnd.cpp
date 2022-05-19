416e80e4e9e5aa3ba4410861115a81844efea50a
Shubham Chandwani <chandwanishubham@gmail.com>
2022-05-20 00:25:27 +0530

    move a character to end of string

---
+++ Recursion/moveXtoEnd.cpp
@@ -0,0 +1,33 @@
+// Move all ‘x’ to the end of the string
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+string moveAllX(string s){
+        if (s.length()==0)
+        {
+            return "";
+        }
+        
+        char ch=s[0];
+        string ans=moveAllX(s.substr(1));
+
+        if (ch=='x')
+        {
+            return (ans+ch);
+        }
+        return ch+ans;
+
+}
+
+int main()
+{
+
+    cout<<moveAllX("aaaxxxexexxxxxxeefxffxffjjjgg");
+    
+    return 0;
+}
