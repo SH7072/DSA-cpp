eadac7521953ede2eff3db6ebcdc8c90e135b473
Shubham Chandwani <chandwanishubham@gmail.com>
2022-06-09 18:00:20 +0530

    find 2 unique numbers in an array where all numbers

--- 3_Bitmanipulation/findUnique2.cpp
+++ 3_Bitmanipulation/findUnique2.cpp
@@ -1,28 +1,69 @@
 // Write a program to find 2 unique numbers in an array where all numbers
 // except two, are present twice.
+
+// Logic
+// 1. Take XOR of all the elements and let that xor value be x. All the repeating
+// elements will get nullified and xor of only two unique elements will last. (as
+// a⊕a = 0).
+// 2. There will be at least one bit set in x. Using that set bit, divide the original
+// set of numbers into two sets.
+// a. First set which contains all the elements with that bit set.
+// b. Second set which contains all the elements with that bit unset.
+// 3. Take xor of both the sets individually, and let those xor values be x1 and x2.
+// 4. Voila, x1 and x2 are our unique numbers. Reason: both the above sets
+// contain one of the unique elements and rest elements of the sets occur
+// twice which will get nullified after ⊕ operation.
+
 #include <bits/stdc++.h>
 
 using namespace std;
-int getSetBit(int n){
-    
+
+int setBit(int n,int pos){
+    n=n>>pos;
+    return n&1;
 }
-void fnd2unique(int arr[],int n){
+int getSetBit(int xorSum)
+{
+
+    int pos = 0;
+    int setBit = 0;
+    while (setBit != 1)
+    {
+        setBit = xorSum & 1;
+        xorSum = xorSum >> 1;
+        pos++;
+    }
+    return pos;
+}
+
+void findTwoUniqueNos(int arr[], int n)
+{
+
+    int xorSum;
     for (int i = 0; i < n; i++)
     {
-        int x;
-        x=x^arr[i];
+        xorSum = xorSum ^ arr[i];
     }
-    
+
+    int pos = getSetBit(xorSum);
+
+    int newxor;
+    for (int i = 0; i < n; i++)
+    {
+        if(setBit(arr[i],pos-1)==1){
+            newxor=newxor^arr[i];
+        }
+
+    }
+    int num2=xorSum^newxor;
+    cout<<num2<<" "<<newxor<<endl;
 }
 int main()
 {
 
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+    int arr[10]={2,5,7,8,2,5,7,8,11,3};
+
+    cout<<setBit(4,3)<<endl;
+    findTwoUniqueNos(arr,10) ;
     return 0;
 }
