cc773d4a7703870e2e8a4a2371223e091b3eaab2
Shubham Chandwani <chandwanishubham@gmail.com>
2022-07-20 00:51:56 +0530

    circular LL

---
+++ 9_LinkedList/16_circularLL.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class node
+{
+
+public:
+    node *next;
+    int data;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        n->next = n;
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next = head;
+    head = n;
+}
+
+void insertAtTail(node *&head, int val)
+{
+
+    if (head == NULL)
+    {
+        insertAtHead(head, val);
+        return;
+    }
+
+    node *n = new node(val);
+    node *temp = head;
+
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = n;
+    n->next = head;
+}
+
+void display(node *head)
+{
+
+    node* temp = head;
+
+    do
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != head);
+
+    cout << endl;
+}
+
+int main()
+{
+
+    node *head=NULL;
+
+    int arr1[] = {1, 3, 4, 6, 7, 9};
+
+    int n = sizeof(arr1) / sizeof(int);
+
+    for (int i = 0; i < n; i++)
+    {
+        insertAtTail(head, arr1[i]);
+    }
+
+    display(head);
+    insertAtHead(head,12);
+    
+    display(head);
+
+    return 0;
+}
